ce511b599b4c83758e1ce2db076c5e9351b7db10
rmb51 <rmb51>
2002-11-24 13:55:03 +0000

    Initial Client add

---
+++ pervasive/hermes/src/client/psl/memento/pervasive/hermes/client/ClientHandler/ClientHandler.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace psl.memento.pervasive.hermes.client.ClientHandler
+{
+	/// <summary>
+	/// Summary description for ClientHandler.
+	/// </summary>
+	public class ClientHandler
+	{
+		private string ip;
+
+		public ClientHandler()
+		{
+			//
+			// TODO: Add constructor logic here
+			//
+		}
+
+		public void setIP(string ip)
+		{
+			this.ip = ip;
+		}
+
+	}
+}
