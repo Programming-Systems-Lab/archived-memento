c0210ef84738d4be3d6e728ae8ba72bd28593c9f
ma810 <ma810>
2003-01-18 16:06:14 +0000

    minor updates

--- virtual/server/etc/frax/frax-gui.bat
+++ virtual/server/etc/frax/frax-gui.bat
@@ -1,4 +1,4 @@
 @echo off
-cd ..
-javaw -cp lib/commons-beanutils.jar;lib/commons-collections.jar;lib/commons-digester.jar;lib/commons-httpclient.jar;lib/commons-logging.jar;lib/commons-logging-api.jar;lib/icu4j.jar;lib/jakarta-poi-1.5.1.jar;lib/JConfig.zip;lib/jena.jar;lib/psl-frax.jar;lib/xerces.jar psl.chime4.server.frax.gui.FraxGUI
-cd bin
+cd ..\..
+java -cp .;libs/commons-beanutils.jar;libs/commons-collections.jar;libs/commons-digester.jar;libs/commons-httpclient.jar;libs/commons-logging.jar;libs/commons-logging-api.jar;libs/dom4j.jar;libs/htmlparser.jar;libs/icu4j.jar;libs/jakarta-oro-2.0.6.jar;libs/jakarta-poi-1.5.1.jar;libs/jalopy-1.0b10.jar;libs/jcvsii.jar;libs/jdom.jar;libs/jena.jar;psl-frax.jar;libs/jftp-bea.jar;libs/log4j.jar;libs/mindbright.jar;libs/mysql-connector-java-2.0.14-bin.jar;libs/pgjdbc2.jar;libs/xerces.jar psl.memento.server.frax.gui.FraxGUI
+cd etc\frax
