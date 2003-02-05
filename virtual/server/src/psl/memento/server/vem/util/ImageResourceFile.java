b042b3b3004c4fc4b81cf6307a57e8071ac6a2a0
vs299 <vs299>
2003-02-05 11:49:13 +0000

    no message

--- virtual/server/src/psl/memento/server/vem/util/ImageResourceFile.java
+++ virtual/server/src/psl/memento/server/vem/util/ImageResourceFile.java
@@ -1,4 +1,3 @@
-<<<<<<< ImageResourceFile.java
 /*
  * ImageFileAnalyzer.java
  *
@@ -39,45 +38,3 @@ public class ImageResourceFile extends ResourceFile
 		return mHeight;
 	}
 }
-=======
-/*
- * ImageFileAnalyzer.java
- *
- * Created on May 26, 2002, 3:41 AM
- */
-
-package psl.memento.server.vem.util;
-
-import java.awt.Toolkit;
-import java.awt.Image;
-import java.io.*;
-
-/**
- *	Class for representing a raster image.
- *
- *	@author  Vladislav Shchogolev
- */
-public class ImageResourceFile extends ResourceFile
-{
-	// member variables
-	private int mWidth, mHeight;
-	
-	/** Creates a new instance of ImageFileAnalyzer */
-	public ImageResourceFile(String iRelFilename) throws IOException
-	{
-		super(iRelFilename);
-		
-		// FIXME: determine dimensions here
-		mWidth = 0;
-		mHeight = 0;
-	}
-	
-	public int getWidth() {
-		return mWidth;
-	}
-	
-	public int getHeight() {
-		return mHeight;
-	}
-}
->>>>>>> 1.3
--- virtual/server/src/psl/memento/server/vem/util/ResourceFile.java
+++ virtual/server/src/psl/memento/server/vem/util/ResourceFile.java
@@ -16,7 +16,7 @@ import java.io.File;
  *	Stores meta data about a resource file (either a model or image).
  *
  *	@author  Vladislav Shchogolev
- *	@version $Revision: 1.3 $
+ *	@version $Revision: 1.4 $
  */
 public class ResourceFile {
     
