0a4396468fa2fc08ba0cbf51ec256277df493095
buko <buko>
2003-02-12 08:47:11 +0000

    ether server, 95% done

---
+++ ether/src/psl/memento/ether/Container.java
@@ -0,0 +1,15 @@
+package psl.memento.ether;
+
+/**
+ * Represents the high-level container which hosts a given set
+ * of components.
+ *
+ * @author Buko O. (buko@cs.columbia.edu)
+ * @version 0.1
+ */
+
+public class Container
+{
+
+
+}
